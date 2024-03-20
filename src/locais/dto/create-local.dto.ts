f91a63eeeb5f4a697dfea994edf37189cd0b9793
Igor-Mont <igornathandev@gmail.com>
2024-03-20 14:52:44 -0300

    feat: add CreateLocalDTO

--- src/locais/dto/create-local.dto.ts
+++ src/locais/dto/create-local.dto.ts
@@ -1,1 +1,44 @@
-export class CreateLocalDto {}
+import {
+  IsNumber,
+  IsString,
+  Length,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
+export class CreateLocalDto {
+  @IsNumber()
+  @Min(50)
+  @Max(200000)
+  capacidade: number;
+
+  @IsString()
+  @Length(8, 8)
+  CEP: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(50)
+  logradouro: string;
+
+  @IsNumber()
+  @Min(1)
+  @Max(10000)
+  numero: number;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(25)
+  bairro: string;
+
+  @IsString()
+  @MinLength(3)
+  @MaxLength(25)
+  cidade: string;
+
+  @IsString()
+  @Length(2, 2)
+  UF: string;
+}
