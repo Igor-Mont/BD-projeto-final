a887c079c051670f537295ece78d5632a17091a0
Igor-Mont <igornathandev@gmail.com>
2024-03-20 21:51:51 -0300

    refactor: remove root app features

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LocaisModule } from './locais/locais.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [LocaisModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
