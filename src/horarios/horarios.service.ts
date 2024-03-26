9fd218feffdf8dcb6713e9c123ae80b1bb79006b
Uneto47 <netovasconcelos02@gmail.com>
2024-03-25 21:38:15 -0300

    feat: add create, findAll, FindOne, Delete and update methods

--- src/horarios/horarios.service.ts
+++ src/horarios/horarios.service.ts
@@ -1,26 +1,74 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
 import { CreateHorarioDto } from './dto/create-horario.dto';
 import { UpdateHorarioDto } from './dto/update-horario.dto';
+import { Horario } from './entities/horario.entity';
+import { IHorariosRepository } from './repositories/IHorariosPrismaRepository';
 
 @Injectable()
 export class HorariosService {
-  create(createHorarioDto: CreateHorarioDto) {
-    return 'This action adds a new horario';
+  constructor(private horariosRepository: IHorariosRepository){}
+ 
+  async create({ hora_inicial, hora_final }: CreateHorarioDto): Promise<Horario> {
+    const [hi, mi, si] = hora_inicial.split(":").map(Number)
+    const [hf, mf, sf] = hora_final.split(":").map(Number)
+    const dateInicial = new Date(2024, 0, 1, hi-3, mi, si).toISOString()
+    const dateFinal = new Date(2024, 0, 1, hf-3, mf, sf).toISOString()
+    const horario = await this.horariosRepository.create({
+      hora_inicial: dateInicial,
+      hora_final: dateFinal
+    });
+    console.log(dateInicial, hi, mi, si);
+    return horario;
   }
 
-  findAll() {
-    return `This action returns all horarios`;
+  async findAll(): Promise<Horario[]> {
+    const horarios = await this.horariosRepository.findAll();
+    return horarios;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} horario`;
+  async findOne(id: string): Promise<Horario> {
+    const horario = await this.horariosRepository.findOne(id);
+    if (!horario) {
+      throw new NotFoundException(`Horario não encontrado.`);
+    }
+    return horario;
   }
 
-  update(id: number, updateHorarioDto: UpdateHorarioDto) {
-    return `This action updates a #${id} horario`;
+  async update(id: string, updateHorarioDto: UpdateHorarioDto): Promise<Horario> {
+    if (updateHorarioDto.hora_inicial === undefined || updateHorarioDto.hora_final === undefined) {
+      throw new Error("Hora inicial ou hora final não estão definidas.");
+    }
+    const [hi, mi, si] = updateHorarioDto.hora_inicial.split(":").map(Number)
+    const [hf, mf, sf] = updateHorarioDto.hora_final.split(":").map(Number)
+    const existingHorario = await this.horariosRepository.findOne(id);
+    
+    if (!existingHorario) {
+      throw new NotFoundException(`Horario não encontrado.`);
+    }
+    
+    const dateInicial = new Date(2024, 0, 1, hi-3, mi, si).toISOString()
+    const dateFinal = new Date(2024, 0, 1, hf-3, mf, sf).toISOString()
+  
+    const updatedHorario = await this.horariosRepository.update(id,{
+      hora_inicial: dateInicial,
+      hora_final: dateFinal
+    });
+    return updatedHorario;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} horario`;
+
+  
+
+  async delete(id: string): Promise<void> {
+    const existingHorario = await this.horariosRepository.findOne(id);
+    if (!existingHorario) {
+      throw new NotFoundException(`Horario não encontrado.`);
+    }
+    await this.horariosRepository.delete(id);
   }
 }
