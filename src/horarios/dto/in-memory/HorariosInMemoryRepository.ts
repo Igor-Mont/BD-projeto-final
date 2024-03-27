d751edc79c540b5884f470b4d99bc76e031d1097
Unaldo Santos Vasconcelos Neto <87950587+Uneto47@users.noreply.github.com>
2024-03-27 14:43:57 -0300

    feat: add horarios module (#2)
    * feat: add prismas sql schemas and migrations
    
    * feat: add initial crud horarios entity
    
    * feat: add horarios tests
    
    * feat: add create, findAll, FindOne, Delete and update methods
    
    * feat: add providers horarioService's
    
    * feat: add horarios repositories
    
    * feat: add validation in case of different days
    
    * fix: update test
    
    * feat: add swagger responses and uuid pipe
    
    * fix: update function and validation days
    
    * feat: add isOpicional validator
    
    * fix: change type
    
    * fix: remove validation repository

---
+++ src/horarios/dto/in-memory/HorariosInMemoryRepository.ts
@@ -0,0 +1,41 @@
+import { CreateHorarioDto } from '@src/horarios/dto/create-horario.dto';
+import { UpdateHorarioDto } from '@src/horarios/dto/update-horario.dto';
+import { Horario } from '../../entities/horario.entity';
+import { IHorariosRepository } from '@src/horarios/repositories/IHorariosPrismaRepository';
+import { shallowEqual } from '../../../utils/shallow-equal';
+import { NotFoundException } from '@nestjs/common';
+
+type Optional<T> = { [K in keyof T]?: T[K] };
+
+export class HorariosInMemoryRepository implements IHorariosRepository {
+  private horarios: Horario[] = [];
+
+  async create(createHorarioDto: CreateHorarioDto): Promise<Horario> {
+    const newHorario = new Horario();
+    Object.assign(newHorario, { ...createHorarioDto });
+
+    this.horarios.push(newHorario);
+    return newHorario;
+  }
+
+  async findAll(): Promise<Horario[]> {
+    return [...this.horarios];
+  }
+
+  async findOne(id: string): Promise<Horario | null> {
+    const horario = this.horarios.find((currentHorario) => currentHorario.id === id);
+    return horario ?? null;
+  }
+
+  async delete(id: string): Promise<void> {
+    this.horarios = this.horarios.filter((horario) => horario.id !== id);
+  }
+
+  async update(id: string, updateHorarioDto: UpdateHorarioDto): Promise<Horario> {
+    const horarioToBeUpdated = (await this.findOne(id)) as Horario;
+
+    Object.assign(horarioToBeUpdated, updateHorarioDto);
+
+    return horarioToBeUpdated;
+  }
+}
---
+++ src/horarios/repositories/IHorariosPrismaRepository.ts
@@ -0,0 +1,15 @@
+import { CreateHorarioDto } from '../dto/create-horario.dto';
+import { UpdateHorarioDto } from '../dto/update-horario.dto';
+import { Horario } from "../entities/horario.entity";
+
+export abstract class IHorariosRepository {
+  abstract create(createHorarioDto: CreateHorarioDto): Promise<Horario>;
+
+  abstract findAll(): Promise<Horario[]>;
+
+  abstract findOne(id: string): Promise<Horario | null>;
+
+  abstract delete(id: string): Promise<void>;
+
+  abstract update(id: string, updateHorarioDto:UpdateHorarioDto): Promise<Horario>;
+}
