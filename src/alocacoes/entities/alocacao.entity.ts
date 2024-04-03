edfb6e898e8c3b0a11d6d077f42bd6f409e9213f
0vitor <w.wutaes@gmail.com>
2024-04-03 12:05:24 -0300

    docs: documentation alocacoes

--- src/alocacoes/entities/alocacao.entity.ts
+++ src/alocacoes/entities/alocacao.entity.ts
@@ -1,10 +1,21 @@
-import { Horario } from '@src/horarios/entities/horario.entity';
-import { Local } from '@src/locais/entities/local.entity';
+import { ApiProperty } from '@nestjs/swagger';
+
+import { Horario } from '../../horarios/entities/horario.entity';
+import { Local } from '../../locais/entities/local.entity';
 
 export class Alocacao {
+  @ApiProperty()
   id: string;
 
+  @ApiProperty()
+  local_id: string;
+
+  @ApiProperty()
   local: Local;
 
+  @ApiProperty()
+  horario_id: string;
+
+  @ApiProperty()
   horario: Horario;
 }
